33a01c8292032d1fb9cc44a0415767078318a7d8
Amr Adel <amradel21@icloud.com>
2020-09-27 21:56:50 +0200

    Express Session Part 1

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
@@ -30,13 +33,23 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+
+//app.use(cookieParser('12345-67890-09876-54321'));
+
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
+
 
 //Authenticate User - Basic
 function auth (req, res, next) {
-  console.log(req.signedCookies);
+  console.log(req.session);
 
-  if (!req.signedCookies.user) {
+  if (!req.session.user) {
     var authHeader = req.headers.authorization;
 
     if (!authHeader) {
@@ -52,7 +65,8 @@ function auth (req, res, next) {
   var pass = auth[1];
 
   if (user == 'admin' && pass == 'password') {
-        res.cookie('user','admin', {signed: true} );
+        //res.cookie('user','admin', {signed: true} );
+        req.session.user = 'admin';
         next(); // authorized
   } 
   else {
@@ -64,7 +78,7 @@ function auth (req, res, next) {
 
   }
   else{
-    if (req.signedCookies.user === 'admin') {
+    if (req.session.user === 'admin') {
       next();
          }
     else {
