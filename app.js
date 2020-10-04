8bf573c127ad75cbfc2be299bf471a04bf6b38dc
Amr Adel <amradel21@icloud.com>
2020-10-04 15:51:32 +0200

    File Upload

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+const uploadRouter = require('./routes/uploadRouter');
 
 //Database
 const mongoose = require('mongoose');
@@ -90,7 +91,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
-
+app.use('/imageUpload',uploadRouter)
 
 
 
